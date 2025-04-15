323a278b9e20fe2598a1820bbba8e622d6228fe1
Mateusz Rojek <kontakt@rojekmateusz.pl>
2025-04-15 16:03:44 +0200

    Migracja bazy danych

--- MyBoards/Entities/Address.cs
+++ MyBoards/Entities/Address.cs
@@ -2,10 +2,11 @@
 
 public class Address
 {
+    public Guid Id { get; set; }
     public string Country { get; set; }
     public string City { get; set; }
     public string Street { get; set; }
     public string PostalCode { get; set; }
-    public int UserId { get; set; }
+    public Guid UserId { get; set; }
     public User User { get; set; } 
 }
